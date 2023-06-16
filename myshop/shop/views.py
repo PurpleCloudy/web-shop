7585a386646e5bc846d70142a6ff83dd552bf5be
Ilya <forstudy546@gmail.com>
2023-06-16 21:44:04 +0300

    cart for my shop

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpRequest
+from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 def product_list(request:HttpRequest, category_slug=None):
@@ -13,4 +14,5 @@ def product_list(request:HttpRequest, category_slug=None):
 
 def product_detail(request:HttpRequest, id:int, slug:Product.slug):
   product = get_object_or_404(Product, id=id, slug=slug, available=True)
-  return render(request, 'shop/product/detail.html', {'product':product})
+  cart_product_form = CartAddProductForm()
+  return render(request, 'shop/product/detail.html', {'product':product, 'cart_product_form': cart_product_form})
