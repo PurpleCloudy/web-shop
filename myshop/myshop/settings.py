d9da67e5fce0f84f1b2bd1bbdf07be68fdfde84a
xannet <xannet@protonmail.com>
2023-06-16 17:40:12 +0300

    changing desktop

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
