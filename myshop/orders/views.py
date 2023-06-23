229af3c95a06f2184c5d1526b76865e47cd213ed
xannet <xannet@protonmail.com>
2023-06-23 16:06:57 +0300

    finished orders

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -17,7 +17,7 @@ def order_create(request):
           quantity = item['quantity']
         )
       cart.clear()
-      return render(request, 'orders/order/created.html')
+      return render(request, 'orders/order/created.html', {'order':order})
   else:
     form = OrderCreateForm()
   return render(request, 'orders/order/create.html', {'cart':cart, 'form':form})
